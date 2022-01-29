592c1e3730c2cf13e99339f601c7e4e7bc074773
Bolanle Ayodeji <abideklove@gmail.com>
2022-01-29 02:30:52 +0100

    First commit

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'The Dating App';
+
+  constructor(private accountService: AccountService){}
+
+  ngOnInit(){
+    this.setCurrentUser();
+  }
+
+  setCurrentUser(){
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
+  }
+}
